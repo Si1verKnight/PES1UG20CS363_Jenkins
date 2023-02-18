ee98557054d52b62899e46cdf20424d61d6adf98
Ubuntu <pes1ug20cs363@pesvd.hgbzneckt4menacm4kbsybw1da.bx.internal.cloudapp.net>
2023-02-18 18:29:19 +0000

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World! \n";
   cout << "Hello, Jenkins! \n";
   cout << "I have successfully built and run. \n";
+  cout << "A git pull a day keeps the conflicts away :) \n";
+  cout << "May the forks be with you! \n";
   return 0;
 }
