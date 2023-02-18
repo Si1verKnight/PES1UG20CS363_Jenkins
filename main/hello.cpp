57c350cab56b878590e2da9c4a66b2b6374d06d7
Sk <pes1ug20cs363@pesvd.hgbzneckt4menacm4kbsybw1da.bx.internal.cloudapp.net>
2023-02-18 18:50:11 +0000

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World! \n";
   cout << "Hello, Jenkins! \n";
-  cout << "I have successfully built and run. \n";
+  cout << "I have successfully built and run. \n");
   cout << "A git pull a day keeps the conflicts away :) \n";
-  cout << "May the forks be with you! \n";
+  cout << "May the forks be with you!" << endl;
   return 0;
 }
