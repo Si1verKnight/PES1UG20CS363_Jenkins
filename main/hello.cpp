2b6508d84d02d0bc293d56cda7705122f5956e49
Si1verKnight <saakshi.sanjeev@yahoo.com>
2023-02-18 19:40:55 +0000

    Updated hello.cpp again

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World! \n";
   cout << "Hello, Jenkins! \n";
   cout << "A git pull a day keeps the conflicts away :) \n";
-  cout << "May the forks be with you!" << endl;
+  cout << "May the forks be with you! \n";
+  cout << " -Sk" << endl;
   return 0;
 }
