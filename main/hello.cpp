a841d68857a695ee55c4670e685a486387bf324d
Si1verKnight <saakshi.sanjeev@yahoo.com>
2023-02-19 09:13:31 +0000

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World! \n";
   cout << "Hello, Jenkins! \n";
-  cout << "A git pull a day keeps the conflicts away :) \n";
-  cout << "May the forks be with you!" << endl;
+  cout << "I have successfully built and run." << endl;
   return 0;
 }
